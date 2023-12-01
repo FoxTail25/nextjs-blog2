b870e8da9f4b4fc1127193072e8452f595cc9e24
Grigoriy <foxtail25@gmail.com>
2023-12-01 10:10:33 +0300

    newPostFromNext

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+
+
+    return <Component {...pageProps} />;
+  }
---
+++ pages/posts/[[...post]].js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+import Layout from "../../components/layout";
+import { useRouter } from "next/router";
+
+export default function FirstPost() {
+
+const router = useRouter()
+let arrPosts = [1,2,3,4,5]
+console.log(router.query.post)
+let post = arrPosts.includes(+router.query.post)
+
+console.log(post)
+
+    return(
+<Layout>
+<Head>
+    <title>First post</title>
+</Head>
+ {post
+ ? <h1>Post № {router.query.post}</h1>
+ : <h1>Этот пост ещё не написали</h1>
+ }
+     <h2><Link href="/">back to home</Link></h2>
+     {/* <h2> Route </h2> */}
+</Layout>
+    )
+}
