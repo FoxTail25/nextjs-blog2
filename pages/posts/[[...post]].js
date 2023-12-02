6e7ce8d4b4d675aad44225e85cafe025203ca8f6
Fox Tail <foxtail25@gmail.com>
2023-12-02 14:07:34 +0300

    fetch

--- pages/posts/[[...post]].js
+++ pages/posts/[[...post]].js
@@ -1,29 +1,54 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
 import Layout from "../../components/layout";
 import { useRouter } from "next/router";
+import userController from "../../lib/users";
+import test from "../../lib/request";
 
-export default function FirstPost() {
+export async function getServerSideProps() {
 
-const router = useRouter()
+    let answerBd = await userController.getUsers()
 
-let arrPosts = [1,2,3,4,5]
-let post = arrPosts.includes(+router.query.post)
+    return {
+        props: {
+            answerBd,
+        },
+    };
+}
 
-console.log("post is : ", post)
 
-    return(
-<Layout>
-<Head>
-    <title>First post</title>
-</Head>
- {post
- ? <h1>Post № {router.query.post}</h1>
- : <h1>Этот пост ещё не написали</h1>
- }
-     <h2><Link href="/">back to home</Link></h2>
-     {/* <h2> Route </h2> */}
-</Layout>
+export default function FirstPost({ answerBd }) {
+
+    console.log(answerBd)
+
+    const router = useRouter()
+
+    let arrPosts = [1, 2, 3, 4, 5]
+    let post = arrPosts.includes(+router.query.post)
+
+    async function test222() {
+        let answer = await test()
+        console.log(answer)
+    }
+
+    return (
+        <Layout>
+            <Head>
+                <title>First post</title>
+            </Head>
+            {post
+                ? <h1>Post № {router.query.post}</h1>
+
+                : <h1>Этот пост ещё не написали</h1>
+            }
+            <h2><Link href="/">back to home</Link></h2>
+            <ul>
+                {answerBd.map(e => <li key={e.id}>{`${e.name} ${e.surname}`}
+                    <button onClick={test222}>
+                        del
+                    </button>
+                </li>)}
+            </ul>
+        </Layout>
     )
 }
